c623eddf1e30961e960c2af71e67cfe7a87545b1
Diego Calderon <diego.calderon_end@yahoo.com>
2022-09-29 01:00:00 -0500

    no funciona como deberia

--- calc.rb
+++ calc.rb
@@ -1,3 +1,21 @@
 # vamos a hacer una calculadora de terminal
 # vamos calcular mruv
-puts "hola mundo"
+
+#vamos a propar con algo simple
+#calcular mruv
+#Vf, Vo, a, t, d
+
+vf = 50
+#vo = 2
+#d = 5
+a = 9.8
+t = ("%.2f" %(vf / a)).to_i
+
+
+#vf = vo + (a * t)
+
+puts "#{t}"
+
+vo = ("%.2f" %(a * t)).to_i - vf
+
+puts "#{vo}"
